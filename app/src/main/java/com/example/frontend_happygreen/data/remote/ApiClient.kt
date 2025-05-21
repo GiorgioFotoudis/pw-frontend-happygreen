eff37c569ca750c7d6ebc65d85ec5aa79aacee37
bertoldi <9698201@itisrossi.vi.it>
2025-05-21 11:35:40 +0200

    IP

--- app/src/main/java/com/example/frontend_happygreen/data/remote/ApiClient.kt
+++ app/src/main/java/com/example/frontend_happygreen/data/remote/ApiClient.kt
@@ -10,7 +10,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 
 object ApiClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8000/api/" // per emulatore Android
+    private const val BASE_URL = "http://172.17.5.206:8000/api/" // per emulatore Android
 
     private val contentType = "application/json".toMediaType()
 
